8da7c93d7db27079d57735172dbbf586921b023d
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-14 15:52:35 +0100

    Add delete and update

--- models/bounty.rb
+++ models/bounty.rb
@@ -1,11 +1,68 @@
+require 'pg'
+
 class Bounty
   attr_accessor :name, :species, :favourite_weapon, :bounty_value
   attr_reader :id
 
   def initialize(options)
+    @id = options['id'].to_i if options['id']
     @name = options['name']
     @species = options['species']
     @favourite_weapon = options['favourite_weapon']
     @bounty_value = options['bounty_value'].to_i
+  end
+
+  def save
+    db = PG.connect({ dbname: 'bounty', host: 'localhost' })
+    sql = "INSERT INTO bounties
+    (name, species, favourite_weapon, bounty_value)
+    VALUES
+    ($1, $2, $3, $4) RETURNING *"
+    values = [@name, @species, @favourite_weapon, @bounty_value]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]['id'].to_i
+    db.close
+  end
+
+  def delete
+    db = PG.connect({ dbname: 'bounty', host: 'localhost' })
+    sql = "DELETE FROM bounties
+    WHERE id = $1"
+    values = [@id]
+    db.prepare("delete_one", sql)
+    db.exec_prepared("delete_one", values)
+    db.close
+  end
+
+  def update
+    db = PG.connect({ dbname: 'bounty', host: 'localhost' })
+    sql = "UPDATE bounties
+    SET
+    (name, species, favourite_weapon, bounty_value) =
+    ($1, $2, $3, $4)
+    WHERE id = $5"
+    values = [@name, @species, @favourite_weapon, @bounty_value, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close
+  end
+
+  def self.delete_all
+    db = PG.connect({ dbname: 'bounty', host: 'localhost' })
+    sql = "DELETE FROM bounties"
+    db.prepare("delete_all", sql)
+    db.exec_prepared("delete_all")
+    db.close
+  end
+
+  def self.find_by_name(bounty_name)
+    db = PG.connect({ dbname: 'bounty', host: 'localhost' })
+    sql = "SELECT FROM bounties
+    WHERE name = '#{bounty_name}'"
+    db.prepare("find_by_name", sql)
+    found = db.exec_prepared("find_by_name")
+    db.close
+
+    return found[0]
   end
 end
